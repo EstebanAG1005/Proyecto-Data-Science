3caa622a876ff3bd3393ddbeddc09fc5dee7fdd3
Juan Carlos Bajan <baj20109@uvg.edu.gt>
2023-11-16 17:42:15 -0600

    mejorando UI

--- app/app.py
+++ app/app.py
@@ -1,47 +1,3 @@
-# from flask import Flask, request, render_template
-# from tensorflow.keras.models import load_model
-# from tensorflow.keras.preprocessing.image import load_img, img_to_array
-# import numpy as np
-# import pickle
-
-# app = Flask(__name__)
-
-# # Cargar el modelo
-# model = load_model('./vgg16.h5')
-
-# @app.route('/', methods=['GET', 'POST'])
-# def upload_predict():
-
-#     with open('class_mapping.pkl', 'rb') as f:
-#         class_mapping = pickle.load(f)
-
-#     # Invertir el mapeo para facilitar la búsqueda por índice
-#     inverted_class_mapping = {v: k for k, v in class_mapping.items()}
-
-        
-#     if request.method == 'POST':
-#         image_file = request.files['image']
-#         if image_file:
-#             image_location = "./images/" + image_file.filename
-#             image_file.save(image_location)
-
-#             img = load_img(image_location, target_size=(224, 224))
-#             img = img_to_array(img)
-#             img = np.expand_dims(img, axis=0)
-#             img = img / 255.0
-
-#             prediction = model.predict(img)
-#             # Aquí puedes procesar la salida de 'prediction' según sea necesario
-#             result = np.argmax(prediction, axis=1)
-#             predicted_class_name = inverted_class_mapping[result[0]]
-#             return render_template('index.html', prediction=predicted_class_name)
-    
-#     return render_template('./index.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 from flask import Flask, request, render_template
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
@@ -52,8 +8,14 @@ import pickle
 
 app = Flask(__name__)
 
-# Cargar el modelo VGG16
-model = load_model('../modelo_entrenado_VGG16.h5')
+# Cargar el modelo
+model_cnn = load_model('./cnn.h5')
+model_vgg16 = load_model('./vgg16.h5')
+
+with open('class_mapping.pkl', 'rb') as f:
+        class_mapping = pickle.load(f)
+
+inverted_class_mapping = {v: k for k, v in class_mapping.items()}
 
 # Cargar el mapeo de clases
 with open('class_mapping.pkl', 'rb') as f:
@@ -61,7 +23,7 @@ with open('class_mapping.pkl', 'rb') as f:
     class_mapping = {v: k for k, v in class_mapping.items()}
 
 # Función para predecir la clase de la imagen
-def predict_class(image_path):
+def predict_class_vgg16(image_path):
     # Cargar la imagen y preprocesarla
     img = load_img(image_path, target_size=(224, 224))
     img = img_to_array(img)
@@ -79,25 +41,44 @@ def predict_class(image_path):
     flattened_features = flattened_features / flattened_features.max()
 
     # Hacer la predicción
-    prediction = model.predict(flattened_features)
+    prediction = model_vgg16.predict(flattened_features)
     result = np.argmax(prediction, axis=1)
     print("Resultado de la predicción:", result)
     print("Mapeo de clases:", class_mapping)
 
     return class_mapping[result[0]]
+
+def predict_class_cnn(image_path):
+    img = load_img(image_path, target_size=(224, 224))
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0
+    prediction = model_cnn.predict(img)
+    # Aquí puedes procesar la salida de 'prediction' según sea necesario
+    result = np.argmax(prediction, axis=1)
+    predicted_class_name = inverted_class_mapping[result[0]]
+    return predicted_class_name  # Just return the result
     
 @app.route('/', methods=['GET', 'POST'])
 def upload_predict():
+    predicted_class = None  # Initialize with None
+
     if request.method == 'POST':
         image_file = request.files['image']
+        model_choice = request.form.get('model_choice')
+
         if image_file:
             image_path = os.path.join('images', image_file.filename)
             image_file.save(image_path)
 
-            predicted_class = predict_class(image_path)
-            return render_template('index.html', prediction=predicted_class)
+            if model_choice == 'cnn':
+                predicted_class = predict_class_cnn(image_path)
+            elif model_choice == 'vgg16':
+                predicted_class = predict_class_vgg16(image_path)
+            else:
+                predicted_class = "Modelo no seleccionado correctamente."
 
-    return render_template('index.html')
+    return render_template('index.html', prediction=predicted_class)
 
 if __name__ == '__main__':
     app.run(debug=True)
