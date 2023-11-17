0bca9d946ec20ea41356e4e08b2e728e737c41fb
Juan Carlos Bajan <baj20109@uvg.edu.gt>
2023-11-17 11:26:20 -0600

    registro de predicciones

--- pruebas_autogluon.py
+++ pruebas_autogluon.py
@@ -21,12 +21,26 @@ def preparar_imagen_para_modelo_tabular(ruta_imagen, modelo, target_size=(224, 2
 # Cargar el modelo
 modelo1 = TabularPredictor.load("AutogluonModels/ag-20231116_225431", require_py_version_match=False)
 
-# Ruta al directorio con imágenes
-ruta_directorio = '../Proyecto-Data-Science-Tem/dataset/throwin/'
+classes = ["play", "challenge", "throwin"]
 
-# Iterar sobre todas las imágenes en el directorio
-for filename in os.listdir(ruta_directorio):
-    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):  # Asegurar que es una imagen
-        ruta_completa = os.path.join(ruta_directorio, filename)
-        prediccion = preparar_imagen_para_modelo_tabular(ruta_completa, modelo1)
-        print(f"Imagen: {filename} - Predicción: {prediccion}")
+for _class in classes:
+
+    # Ruta al directorio con imágenes
+    ruta_directorio = f'../Proyecto-Data-Science-Tem/dataset/{_class}/'
+
+    lines = []
+
+    # Iterar sobre todas las imágenes en el directorio
+    for filename in os.listdir(ruta_directorio):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):  # Asegurar que es una imagen
+            ruta_completa = os.path.join(ruta_directorio, filename)
+            prediccion = preparar_imagen_para_modelo_tabular(ruta_completa, modelo1)
+            lines.append((prediccion, filename))
+
+    # Convertir la lista de tuplas en un DataFrame
+    df = pd.DataFrame(lines, columns=['predicted_class', 'filename'])
+
+    # Escribir el DataFrame a un archivo Excel
+    nombre_del_archivo = f'./autogluon/{_class}.xlsx'
+    df.to_excel(nombre_del_archivo, index=False)
+
--- pruebas_cnn.py
+++ pruebas_cnn.py
@@ -11,39 +11,34 @@ import os
 import pickle
 import time
 
-model_vgg16 = load_model('./app./VGG16.h5')
+model_cnn = load_model('./app./cnn.h5')
 
+with open('./app/class_mapping.pkl', 'rb') as f:
+        class_mapping = pickle.load(f)
+
+inverted_class_mapping = {v: k for k, v in class_mapping.items()}
+
+# Cargar el mapeo de clases
 with open('./app/class_mapping.pkl', 'rb') as f:
     class_mapping = pickle.load(f)
     class_mapping = {v: k for k, v in class_mapping.items()}
-    inverted_class_mapping = {v: k for k, v in class_mapping.items()}
 
-# Función para predecir la clase de la imagen
-def predict_class_vgg16(image_path):
+
+def predict_class_cnn(image_path):
     start_time = time.time()
-    # Cargar la imagen y preprocesarla
     img = load_img(image_path, target_size=(224, 224))
     img = img_to_array(img)
     img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img)
-
-    # Obtener características usando VGG16 (sin las capas superiores)
-    base_model = VGG16(weights='imagenet', include_top=False)
-    features = base_model.predict(img)
-
-    # Aplanar las características para coincidir con el entrenamiento
-    flattened_features = features.reshape(1, 7*7*512)
-
-    # Normalizar los píxeles
-    flattened_features = flattened_features / flattened_features.max()
-
-    # Hacer la predicción
-    prediction = model_vgg16.predict(flattened_features)
+    img = img / 255.0
+    prediction = model_cnn.predict(img)
+    # Aquí puedes procesar la salida de 'prediction' según sea necesario
     result = np.argmax(prediction, axis=1)
+    predicted_class_name = inverted_class_mapping[result[0]]
     end_time = time.time()
     prediction_time = end_time - start_time
-    model="VGG16"
-    return class_mapping[result[0]], prediction_time, model
+    model="CNN"
+    return predicted_class_name, prediction_time, model  # Just return the result
+
 
 classes = ["play", "challenge", "throwin"]
 
@@ -58,12 +53,12 @@ for _class in classes:
     for filename in os.listdir(ruta_directorio):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg')):  # Asegurar que es una imagen
             ruta_completa = os.path.join(ruta_directorio, filename)
-            predicted_class, prediction_time, model = predict_class_vgg16(ruta_completa)
+            predicted_class, prediction_time, model = predict_class_cnn(ruta_completa)
             lines.append((predicted_class, filename))
 
     # Convertir la lista de tuplas en un DataFrame
     df = pd.DataFrame(lines, columns=['predicted_class', 'filename'])
 
     # Escribir el DataFrame a un archivo Excel
-    nombre_del_archivo = f'{_class}.xlsx'
+    nombre_del_archivo = f'./cnn/{_class}.xlsx'
     df.to_excel(nombre_del_archivo, index=False)
